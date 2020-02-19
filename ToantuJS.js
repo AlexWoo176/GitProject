24d63de087c103611e85ab0ce317c428da0eeed4
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-02-19 11:47:10 +0700

    add ToantuJS.js

---
+++ ToantuJS.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the With");
+inputHeight = prompt(" Enter the Hight");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area   = width * height;
+document.write("The is area:"+area);
