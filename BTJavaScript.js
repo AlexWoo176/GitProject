8d71dd6ca77bab013047b8d13fd9fa592d31f757
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-02-19 15:59:24 +0700

    add BTJavaScript

---
+++ BTJavaScript.js
@@ -0,0 +1,24 @@
+
+let diemvatly = parseInt(prompt("Nhập vào điểm Vật Lý"));
+let diemhoa = parseInt(prompt("Nhập vào điểm Hóa"));
+let diemtoan = parseInt(prompt("Nhập vào điểm Toán"));
+
+let DiemTong= diemvatly + diemtoan + diemhoa;
+let DiemTrungBinh = DiemTong/3;
+
+alert("DiemTong:"+DiemTong);
+alert("DiemTrungBinh:"+DiemTrungBinh);
+// Bài Tập Tính Điểm Tổng và Điểm Trung Bình
+
+let DoCelsius = parseInt(prompt("Nhập vào Độ C"));
+let DoFahrenheit = (1.8*DoCelsius)+32;
+alert("Độ Fahrenheit:"+DoFahrenheit);
+// Bài tập Chuyển đổi độ C sang độ F
+
+let bankinh =parseInt(prompt("Nhập bán kính"));
+let chuvihinhtron = 2*bankinh*3.14;
+let dientichhinhtron = bankinh*bankinh*3.14;
+alert("Chu Vi Hình Tròn:" +chuvihinhtron);
+alert("Diện Tích Hình Tròn:"+ dientichhinhtron);
+// Bài tập tính chu vi và diện tích hình tròn
+
